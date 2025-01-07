438dd0b239ee63c1cc30c23b81809134f1cd95ea
aditisunil <aditisunil13@gmail.com>
2025-01-06 22:00:53 -0600

    headshot

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,10 +4,11 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters'; 
 import Loader from 'react-loaders';
+import Headshot from '../../assets/images/headshot.jpeg';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const helloArray = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'I', "'", 'm', ' '];
+  const helloArray = ['H', 'e', 'l', 'l', 'o', '!', ' ', 'I', "'", 'm', ' '];
   const nameArray = ['A', 'd', 'i', 't', 'i', ' ', 'S', 'u', 'n', 'i', 'l', ','];
   const jobArray = ['a', ' ', 'S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
 
@@ -20,25 +21,22 @@ const Home = () => {
 
   return (
     <>
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-
-          <AnimatedLetters letterClass={letterClass} strArray={helloArray} idx={11} />
-          <br />
-
-          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={8} />
-          <br />
-
-          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} />
-
-        </h1>
-        <h2>Frontend Developer / UI / UX</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters letterClass={letterClass} strArray={helloArray} idx={11} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={8} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} />
+          </h1>
+          <h2>Frontend Developer / UI / UX / AI</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <img src={Headshot} alt="Aditi Sunil" className="headshot" />
       </div>
-    </div>
     <Loader type="pacman"/>
     </>
   );
